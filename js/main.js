1602f3b7354364127a3cc4e5b82ec7588951c5eb
karen <karendecleva3@gmail.com>
2023-08-20 15:48:59 -0400

    ajustes

--- js/main.js
+++ js/main.js
@@ -8,28 +8,34 @@ window.onload = () => {
 function calcular() {
     const peso = document.getElementById("peso").value;
     const altura = document.getElementById("altura").value;
+    const nome = document.getElementById("nome").value;
+    const sexo = document.getElementById("sexo").value;
+    const idade = document.getElementById("idade").value;
   
     calculo = peso / (altura * altura);
-
+    
+    nomeResultado.textContent = nome;
+    idadeResultado.textContent = idade;
+    sexoResultado.textContent = sexo;
     alturaResultado.textContent = altura + "m";
     pesoResultado.textContent = peso + "kg";
     IMCresultado.textContent = Math.floor(calculo);
   
     if (calculo < 18.5) {
-    tabela.textContent = "abaixo do peso";} 
+    classificacao.textContent = "abaixo do peso";} 
     else if ((calculo > 18.5) & (calculo < 24.9)) {
-    tabela.textContent = "peso normal";}
+    classificacao.textContent = "peso normal";}
 
     else if ((calculo > 25.0) & (calculo < 29.9)) {
-    tabela.textContent = "excesso de peso";} 
+    classificacao.textContent = "excesso de peso";} 
       
     else if ((calculo > 30.0) & (calculo < 34.9)) {
-    tabela.textContent = "obsidade classe I";} 
+    classificacao.textContent = "obsidade classe I";} 
     
     else if ((calculo > 35.0) & (calculo < 39.9)) {
-    tabela.textContent = "obsidade classe II";} 
+    classificacao.textContent = "obsidade classe II";} 
 
     else if (calculo > 40) {
-    tabela.textContent = "obsidade classe III";}
+    classificacao.textContent = "obsidade classe III";}
   }
   
