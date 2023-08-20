94cb66963747a9007cdee9442a08a9fcd8e78718
karen <karendecleva3@gmail.com>
2023-08-19 20:20:21 -0400

    calculo funcionando

--- js/main.js
+++ js/main.js
@@ -4,3 +4,32 @@ window.onload = () => {
         navigator.serviceWorker.register("./sw.js");
     }
 };
+
+function calcular() {
+    const peso = document.getElementById("peso").value;
+    const altura = document.getElementById("altura").value;
+  
+    calculo = peso / (altura * altura);
+
+    alturaResultado.textContent = altura + "m";
+    pesoResultado.textContent = peso + "kg";
+    IMCresultado.textContent = Math.floor(calculo);
+  
+    if (calculo < 18.5) {
+    tabela.textContent = "abaixo do peso";} 
+    else if ((calculo > 18.5) & (calculo < 24.9)) {
+    tabela.textContent = "peso normal";}
+
+    else if ((calculo > 25.0) & (calculo < 29.9)) {
+    tabela.textContent = "excesso de peso";} 
+      
+    else if ((calculo > 30.0) & (calculo < 34.9)) {
+    tabela.textContent = "obsidade classe I";} 
+    
+    else if ((calculo > 35.0) & (calculo < 39.9)) {
+    tabela.textContent = "obsidade classe II";} 
+
+    else if (calculo > 40) {
+    tabela.textContent = "obsidade classe III";}
+  }
+  
--- sw.js
+++ sw.js
@@ -1,19 +1,18 @@
-let cacheName = "my-first-pwa";
-let filesCache = ["/","/index.html",
-                   "/css/style.css", "/js/main.js"];
+let cacheName = "imc";
+let filesToCache = ["/", "/index.html", "/css/style.css", "/js/main.js"];
 
 self.addEventListener("install", (e) => {
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
-            return cache.addAll(filesCache);
-        })
+      caches.open(cacheName).then(function (cache) {
+        return cache.addAll(filesToCache);
+      })
     );
-});
+  });
 
-self.addEventListener("fetch", (e) => {
+  self.addEventListener("fetch", (e) => {
     e.respondWith(
-        caches.match(e.request).then((response) => {
-            return response || fetch(e.request);
-        })
+      caches.match(e.request).then((response) => {
+        return response || fetch(e.request);
+      })
     );
-});
+  });
